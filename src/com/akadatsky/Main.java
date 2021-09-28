7a5e5ee77522b3db1eba59975d90fca2bd35e5e5
Andrey Kadatsky <andrey.kadatsky@gmail.com>
2021-09-28 21:12:54 +0300

    Add comment

--- src/com/akadatsky/Main.java
+++ src/com/akadatsky/Main.java
@@ -2,9 +2,13 @@ package com.akadatsky;
 
 public class Main {
 
+    
+    // test
     public static void main(String[] args) {
-
         System.out.println("Hello Java!!!1111");
-
     }
+
+
+
+
 }
